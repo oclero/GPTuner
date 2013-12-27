7bcabd447d4c2ce113b0c28bfc35bde45daabaa7
Olivier Cléro <oclero@hotmail.com>
2013-12-27 01:02:54 +0100

    WPF basis

--- GPTuner/Tuner/MainWindow.xaml.cs
+++ GPTuner/Tuner/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Tuner
 {
@@ -20,9 +24,73 @@ namespace Tuner
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region playedStrings
+        private bool[] playedStrings;
+        public bool[] PlayedStrings
+        {
+            get { return playedStrings; }
+            set { NotifyPropertyChanged(ref playedStrings, value); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string nomPropriete)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nomPropriete));
+        }
+
+        private bool NotifyPropertyChanged<T>(ref T variable, T valeur, [CallerMemberName] string nomPropriete = null)
+        {
+            if (object.Equals(variable, valeur)) return false;
+
+            variable = valeur;
+            NotifyPropertyChanged(nomPropriete);
+            return true;
+        }
+        #endregion
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            playedStrings = new bool[6]; // false by default
+            CreateSaveCommand();
         }
+        #endregion
+
+        #region Command : Jouer les cordes
+        public ICommand PlayStringsCommand
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Possible uniquement si au moins 1 corde est jouée
+        /// </summary>
+        private bool CanExecutePlayStringsCommand()
+        {
+            foreach (bool value in playedStrings){
+                if(value){ return true;}
+            }
+            return false;
+        }
+
+        public void PlayStringsExecute()
+        {
+            // TODO
+        }
+
+        private void CreateSaveCommand()
+        {
+            PlayStringsCommand = new RelayCommand(PlayStringsExecute, CanExecutePlayStringsCommand);
+        }
+
+
+        #endregion
+
     }
+
 }
---
+++ GPTuner/Tuner/Model/TunerModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPTunerWrapper;
+
+namespace Tuner.Model
+{
+    class TunerModel
+    {
+        #region Members
+        private TunerWrapper Tuner;
+        #endregion
+
+        #region Constructeur
+        public TunerModel()
+        {
+            Tuner = new TunerWrapper();
+        }
+        #endregion
+
+        #region Methods
+        public float GetError(int guitarString)
+        {
+            return Tuner.getError(guitarString);
+        }
+
+        public float[] GetErrorsForAllStrings()
+        {
+            float[] errors = new float[6];
+            for (int i = 0; i < 6; i++)
+            {
+                errors[i] = GetError(i);
+            }
+            return errors;
+        }
+        #endregion
+    }
+}
